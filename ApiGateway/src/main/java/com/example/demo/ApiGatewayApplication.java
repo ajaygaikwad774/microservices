b3f35dd591d2fa9d3b8fe7f5cdad6967f8b1e535
ajaygaikwad774 <ajaygaikwad774@gmail.com>
2020-02-16 19:16:54 +0530

    second commit by ajay

--- ApiGateway/src/main/java/com/example/demo/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulServer
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
